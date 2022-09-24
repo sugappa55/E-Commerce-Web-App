1301a13894341abac3f067f00edc6cb5a2a69545
sugappa55 <kurubasugappa@gmail.com>
2022-09-24 21:48:15 +0530

    product page designed

--- src/Components/EntityCard.jsx
+++ src/Components/EntityCard.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
-import { useState } from 'react'
+import { Rating } from "@mui/material";
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const EntityCard = ({product}) => {
-  let [image,setImage]=useState(product.images[0])
-  const onHover=()=>{
-    setImage(product.images[1])
-  }
-  const onHoverOut=()=>{
-    setImage(product.images[0])
-  }
+const EntityCard = ({ product }) => {
+  const navigate = useNavigate();
+  let [image, setImage] = useState(product.images[0]);
+  const onHover = () => {
+    setImage(product.images[1]);
+  };
+  const onHoverOut = () => {
+    setImage(product.images[0]);
+  };
 
   return (
-    <div onMouseEnter={onHover} onMouseLeave={onHoverOut}>
-      <img src={image} alt="data" />
-      <p>{product.name}|{product.color}|{product.gender}</p>
-      
+    <div
+      onMouseEnter={onHover}
+      onMouseLeave={onHoverOut}
+      onClick={() => navigate(`/single/${product.id}`)}
+      className="cursor-pointer"
+    >
+      <img src={image} alt="data" className="w-full md:h-56 h-64 object-cover" />
+      <div className="mt-4">
+        {" "}
+        <p className="text-ellipsis overflow-hidden text-center md:text-xs py-4">
+          {product.name} | {product.color} | {product.gender}
+        </p>
+        <div className="flex flex-wrap justify-center">
+          {" "}
+          <Rating
+            style={{ color: "#ffc107" }}
+            value={product.rating > 0 ? product.rating : 4}
+            readOnly
+          />
+          <p>
+            {product.reviews ?? 2} {"  "}reviews
+          </p>
+        </div>
+        <p className="text-gray-600 py-2 text-center">
+          RS {product.final_price}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default React.memo(EntityCard)
+export default React.memo(EntityCard);
--- src/Pages/AllEntities/AllEntities.jsx
+++ src/Pages/AllEntities/AllEntities.jsx
@@ -1,18 +1,122 @@
-import React from 'react'
-import { useEffect } from "react";
+import React from "react";
 
-import {useDispatch, useSelector} from "react-redux"
-import {GetData} from "../../Redux/Actions"
+import { useSelector } from "react-redux";
+import { MdExpandMore, MdOutlineFilterList } from "react-icons/md";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import EntityCard from "../../Components/EntityCard"
 
 const AllEntities = () => {
   const data = useSelector((store) => store.products);
 
-  
   return (
-    <div>
-      all
+    <div className="px-16 py-4">
+      <div>
+        <h2 className="text-3xl font-semibold py-6">Shop All</h2>
+        <div id="filter" className="flex md:justify-end justify-between">
+          <button className="md:hidden">
+            {" "}
+            <MdOutlineFilterList className="mr-2  text-xl inline" />
+            Filters{" "}
+          </button>
+          <button>
+            Sort:Some value <MdExpandMore className="ml-1 text-xl inline" />
+          </button>
+        </div>
+        <div className="py-12 flex w-full gap-2">
+          <div className="hidden md:block w-[25%]">
+            <Accordion style={{ borderBottom: "1px solid" }}>
+              <AccordionSummary
+                expandIcon={<MdExpandMore className="text-3xl" />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <p className="text-sm uppercase font-semibold">Size</p>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div className="flex justify-start items-center gap-2">
+                  <input type="checkbox" name="size" id="" />
+                  <label htmlFor="size">5</label>
+                  <br />
+                </div>
+                <div className="flex justify-start items-center gap-2">
+                  <input type="checkbox" name="size" id="" />
+                  <label htmlFor="size">6</label>
+                  <br />
+                </div>
+                <div className="flex justify-start items-center gap-2">
+                  <input type="checkbox" name="size" id="" />
+                  <label htmlFor="size">7</label>
+                  <br />
+                </div>
+                <div className="flex justify-start items-center gap-2">
+                  <input type="checkbox" name="size" id="" />
+                  <label htmlFor="size">8</label>
+                  <br />
+                </div>
+                <div className="flex justify-start items-center gap-2">
+                  <input type="checkbox" name="size" id="" />
+                  <label htmlFor="size">9</label>
+                  <br />
+                </div>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion style={{ borderBottom: "1px solid",py:2 }}>
+              <AccordionSummary
+                expandIcon={<MdExpandMore className="text-3xl" />}
+              >
+                <p className="font-semibold text-sm uppercase">Price </p>
+              </AccordionSummary>
+            </Accordion>
+            <Accordion style={{ borderBottom: "1px solid",py:2 }}>
+              <AccordionSummary
+                expandIcon={<MdExpandMore className="text-3xl" />}
+              >
+                <p className="font-semibold text-sm uppercase">Gender </p>
+              </AccordionSummary>
+              <AccordionDetails>
+              <div className="flex justify-start items-center gap-2">
+                  <input type="checkbox" name="gender" id="" />
+                  <label htmlFor="gender">Men</label>
+                  <br />
+                </div>
+                <div className="flex justify-start items-center gap-2">
+                  <input type="checkbox" name="gender" id="" />
+                  <label htmlFor="gender">Women</label>
+                  <br />
+                </div>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion style={{ borderBottom: "1px solid",py:2 }}>
+              <AccordionSummary
+                expandIcon={<MdExpandMore className="text-3xl" />}
+              >
+                <p className="font-semibold text-sm uppercase">Collection </p>
+              </AccordionSummary>
+              <AccordionDetails>
+              <div className="flex justify-start items-center gap-2">
+                  <input type="checkbox" name="collections" id="" />
+                  <label htmlFor="collections">Banana clicks</label>
+                  <br />
+                </div>
+                <div className="flex justify-start items-center gap-2">
+                  <input type="checkbox" name="collections" id="" />
+                  <label htmlFor="collections">Softknit Loafers</label>
+                  <br />
+                </div>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          <div className="grid md:grid-cols-3 sm:grid-cols-2 md:w-[75%] w-full gap-2 md:mt-[-3rem]">
+            {
+              data.map(product=>(
+                <EntityCard product={product}/>
+              ))
+            }
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllEntities
+export default AllEntities;
